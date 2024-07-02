504676da867d43d2518bf2da8d3b4ae7799c7d3c
mikhailyatsenko <misha.ggg@gmail.com>
2024-07-02 14:36:52 +0200

    setup gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import { LocationProvider } from 'app/providers/LocationProvider';
 import { ApolloProviderWrapper } from 'app/providers/ApolloProviderWrapper';
 
